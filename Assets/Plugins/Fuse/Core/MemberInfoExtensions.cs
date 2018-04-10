711a6880dd9ad883fe0bf3e90598a83ed1b7ba3f
Jacob Harrison <jdharrison@fractality.co>
2018-04-10 12:49:09 -0700

    Add InstantiateAttribute that manages a UnityEngine.Object assigned to it

---
+++ Assets/Plugins/Fuse/Core/MemberInfoExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Fuse.Core
+{
+	public static class MemberInfoExtensions
+	{
+		public static object GetValue(this MemberInfo target, object instance)
+		{
+			if (target is PropertyInfo)
+			{
+				PropertyInfo propertyInfo = (PropertyInfo) target;
+				propertyInfo.GetValue(instance, null);
+			}
+			else if (target is FieldInfo)
+			{
+				FieldInfo fieldInfo = (FieldInfo) target;
+				fieldInfo.GetValue(instance);
+			}
+
+			throw new NotSupportedException();
+		}
+
+		public static void SetValue(this MemberInfo target, object instance, object value)
+		{
+			if (target is MethodInfo)
+			{
+				MethodInfo methodInfo = (MethodInfo) target;
+				ParameterInfo[] parameters = methodInfo.GetParameters();
+				methodInfo.Invoke(instance, parameters.Select(parameter => value).ToArray());
+			}
+			else if (target is PropertyInfo)
+			{
+				PropertyInfo propertyInfo = (PropertyInfo) target;
+				propertyInfo.SetValue(instance, value, null);
+			}
+			else if (target is FieldInfo)
+			{
+				FieldInfo fieldInfo = (FieldInfo) target;
+				fieldInfo.SetValue(instance, value);
+			}
+
+			throw new NotSupportedException();
+		}
+	}
+}
---
+++ Assets/Plugins/Fuse/Features/InstantiateAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Fuse.Core;
+using JetBrains.Annotations;
+using Object = UnityEngine.Object;
+
+namespace Fuse.Feature
+{
+	/// <summary>
+	/// Instantiates a reference to a <see cref="UnityEngine.Object"/> when entering lifecyle, and destroys it upon exit.
+	/// </summary>
+	[MeansImplicitUse]
+	[DefaultLifecycle(Lifecycle.Active)]
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+	public class InstantiateAttribute : Attribute, IFuseLifecycle
+	{
+		public uint Order { get; set; }
+		public Lifecycle Lifecycle { get; set; }
+
+		public void OnEnter(MemberInfo target, object instance)
+		{
+			Object prefab = (Object) target.GetValue(instance);
+			if (prefab != null)
+			{
+				target.SetValue(instance, Object.Instantiate(prefab));
+			}
+		}
+
+		public void OnExit(MemberInfo target, object instance)
+		{
+			Object.Destroy((Object) target.GetValue(instance));
+			target.SetValue(instance, null);
+		}
+	}
+}
